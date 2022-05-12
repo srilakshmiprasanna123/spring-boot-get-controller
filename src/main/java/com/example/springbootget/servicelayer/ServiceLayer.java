46145460a234f994b71228133e41b9fb7a4fa932
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-05-13 00:34:06 +0530

    added firstname and lastname

--- src/main/java/com/example/springbootget/servicelayer/ServiceLayer.java
+++ src/main/java/com/example/springbootget/servicelayer/ServiceLayer.java
@@ -1,9 +1,17 @@
 package com.example.springbootget.servicelayer;
 
+import com.example.springbootget.entity.User;
 import org.springframework.stereotype.Service;
 @Service
 public class ServiceLayer {
     public String sayHello(){
             return "Hello World!!";
         }
-}
+
+
+        public String sayPostHello(User user) {
+            User newUser = new User(user);
+            return "Hello "+ newUser;
+        }
+    }
+
