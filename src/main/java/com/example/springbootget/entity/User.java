db7625873e17c47090a292cfe053b8593537d580
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-05-13 00:21:17 +0530

    added service layer

--- src/main/java/com/example/springbootget/entity/User.java
+++ src/main/java/com/example/springbootget/entity/User.java
@@ -20,4 +20,5 @@ public class User {
         this.firstName = lastName;
     }
 
+
 }
---
+++ src/main/java/com/example/springbootget/servicelayer/ServiceLayer.java
@@ -0,0 +1,9 @@
+package com.example.springbootget.servicelayer;
+
+import org.springframework.stereotype.Service;
+@Service
+public class ServiceLayer {
+    public String sayHello(){
+            return "Hello World!!";
+        }
+}
